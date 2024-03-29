087c33236c1d5a95f054e0d2e0658a48053ce44d
arjunhm <hm.arjun97@gmail.com>
2024-03-29 17:08:11 -0400

    Added User model

--- core/settings.py
+++ core/settings.py
@@ -22,6 +22,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "user",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -110,3 +112,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "user.User"
