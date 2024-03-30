4ad39d76ab8f32b41741c3a613f8158bbf3e0f33
arjunhm <hm.arjun97@gmail.com>
2024-03-29 22:10:54 -0400

    Added Account

---
+++ account/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+ACCOUNT_CHOICES = (
+    ("checking", "Checking"),
+    ("savings", "Savings"),
+    ("credit_card", "Credit Card"),
+)
+
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="accounts")
+    name = models.CharField(max_length=100)
+    account_type = models.CharField(
+        max_length=20,
+        choices=ACCOUNT_CHOICES,
+    )
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f"{self.name} ({self.account_type})"
--- core/settings.py
+++ core/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "user",
+    "account",
     "api",
     "tests",
     "rest_framework",
