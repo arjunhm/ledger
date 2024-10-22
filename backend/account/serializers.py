b9b9bc7c1cf207c0ef476b7c76b873d3fdb6f4c4
arjunhm <hm.arjun97@gmail.com>
2024-10-22 17:31:03 -0500

    update account serializer

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -6,7 +6,7 @@ from account.models import Account
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ["id", "name", "type", "initial_balance", "is_active", "created_at"]
+        fields = ["id", "name", "type", "balance", "is_active", "created_at"]
         read_only_fields = ["created_at"]
 
     def create(self, validated_data):
