b9db7e9d9e3ba69e28c1ec9758f62b124af982b7
arjunhm <hm.arjun97@gmail.com>
2024-10-22 15:42:52 -0500

    add user urls

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -84,6 +84,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
